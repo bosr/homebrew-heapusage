8a76fb6ab1d8426153c67bdeaf600ffa27dc1025
Romain Bossart <romain.bossart@fastmail.com>
2018-06-05 21:53:29 +0200

    bump to v1.0

--- Formula/heapusage.rb
+++ Formula/heapusage.rb
@@ -1,9 +1,8 @@
 class Heapusage < Formula
   desc "Find memory leaks in Linux and macOS applications"
   homepage "https://github.com/d99kris/heapusage"
-  url "https://github.com/d99kris/heapusage"
-  version "1.0"
-  sha256 "0de9eb9b83e6118879660bb6561696cbeaee5d77e21b4cc98ebe6b2de1c1d8e8"
+  url "https://github.com/d99kris/heapusage/archive/v1.0.tar.gz"
+  sha256 "b37585f52c8d6410f33ad0c5808ebb0d47ff0694891d65e4a63c8501c6817af0"
   head "https://github.com/d99kris/heapusage.git"
 
   depends_on "cmake" => :build
@@ -13,21 +12,9 @@ class Heapusage < Formula
       system "cmake", "-G", "Unix Makefiles", "..", *std_cmake_args
       system "make", "install"
     end
-    # system "cmake", "-G", "Unix Makefiles", ".", *std_cmake_args
-    # system "make", "install"
-
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test heapusage`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "#{bin}/heapusage", "ruby", "--version"
+    system "#{bin}/heapusage", "env"
   end
 end
